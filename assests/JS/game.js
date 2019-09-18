dc882b993214d3e8912b8a3994c6226755379996
Dick James <rnulanz@gmail.com>
2019-09-18 17:34:44 -0400

    Updated

---
+++ assests/JS/game.js
@@ -0,0 +1,50 @@
+
+ var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' ,'o', 'p', 'q', 'r', 's', 't', 'u', 'v' ,'w', 'x', 'y' ,'z'];
+
+ var wins= 0;
+ var losses= 0;
+ var left= 9;
+ var yourletters=[];
+ 
+ var directionsText = document.getElementById("directions-text");
+ var whatImThinkingText = document.getElementById("whatImThinking-text");
+ var winsText = document.getElementById("wins-text");
+ var lossesText = document.getElementById("losses-text");
+ var guessesLeftText = document.getElementById("guessesLeft-text");
+ var guessesSoFarText = document.getElementById("guessesSoFar-text");
+ 
+ document.onkeyup = function(event){
+     var userGuess = event.key;
+     var userGuess2 = userGuess.toLowerCase();
+     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    
+     yourletters.push(userGuess2);
+     
+     if (userGuess2 === computerGuess){
+         wins++;
+     }
+     else {
+         if (left <= 0){
+         losses++;
+         yourletters= [];
+         left = 9;
+
+
+     }
+
+         else {
+             left--;
+         }
+     
+         
+     }
+     directionsText.textContent = "";
+     whatImThinkingText.textContent= "Guess what letter I am thinking!!!";
+     winsText.textContent = "wins " + wins;
+     lossesText.textContent = "losses: " + losses;
+     guessesLeftText.textContent = "Guesses Left " + left;
+     guessesSoFarText.textContent = "Letters " + yourletters;
+ 
+     }
+
+
